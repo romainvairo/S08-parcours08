424901db65a18ef8010e50a78565f6131845ab9c
RomainVairo <romain.vairo@hotmail.com>
2019-08-16 11:35:23 +0200

    quelques modifications

--- singular.php
+++ singular.php
@@ -1,3 +1,21 @@
 <?php
-echo "singular";
-?>
+get_header();
+
+
+if ( have_posts() ) :
+
+
+				while ( have_posts() ) :
+                    the_post();
+                    ?>
+                    <main class='right'>
+<h1><?php the_title(); ?></h1>
+<?php the_content(); ?>
+                </main>
+                <?php
+                endwhile;
+            endif;
+
+
+
+get_footer(); 
